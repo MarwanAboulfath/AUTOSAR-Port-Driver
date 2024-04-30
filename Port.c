896393f997e23b8d946de0fbc2fd128144af0fef
MarwanAboulfath <168493256+MarwanAboulfath@users.noreply.github.com>
2024-04-30 03:08:59 +0300

    Port.c

--- Port.c
+++ Port.c
@@ -6,7 +6,7 @@
  *
  * Description: Source file for TM4C123GH6PM Microcontroller - Port Driver.
  *
- * Author: Marwan
+ * Author: Marwan Aboulfath
  ******************************************************************************/
 
 #include "Port.h"
